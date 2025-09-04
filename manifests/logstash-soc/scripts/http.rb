df801ee866ee8512120efbea131730294e8a1009
pjames <pjames@gmail.com>
2025-09-04 19:51:18 +0700

    Pruned unused fields

--- manifests/logstash-soc/scripts/http.rb
+++ manifests/logstash-soc/scripts/http.rb
@@ -9,6 +9,18 @@ def register(params)
     $stdout.sync = true
 end
 
+def create_fields_from_json(event, objKey)
+    all_fields = event.to_hash
+
+    # set กลับเข้า event โดยห่อใน data
+    event.set(objKey, all_fields)
+
+    # ลบ field อื่นออก ยกเว้น data
+    all_fields.keys.each do |k|
+      event.remove(k) unless k == objKey
+    end
+end
+
 def populate_ts_aggregate(event)
     dtm = DateTime.now
     dtm += Rational('7/24') # Thailand timezone +7
@@ -26,6 +38,8 @@ def filter(event)
     cust_ts_yyyy = event.get('cust_ts_yyyy')
     cust_ts_mm = event.get('cust_ts_mm')
     cust_ts_dd = event.get('cust_ts_dd')
+
+    create_fields_from_json(event, 'data')
 
     full_index_name = "onix-v2-#{cust_ts_yyyy}-#{cust_ts_mm}-#{cust_ts_dd}"
     event.set('index_name', full_index_name)
